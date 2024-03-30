f7b83748499516428eead1705dac2bc314778a99
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-30 20:40:42 -0300

    first sha256 done

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-. 
+ package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	h := sha256.New()
+	h.Write([]byte("this is a password"))
+	// Calculate and print the hash
+	fmt.Printf("%x", h.Sum(nil))
+}
