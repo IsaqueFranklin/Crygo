4ac20869e6c68c1136b2f06950795922da2e2eac
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-04 11:13:24 -0300

    similar example

--- hash.go
+++ hash.go
@@ -9,6 +9,10 @@ func main() {
   h := sha256.New()
   h.Write([]byte("Carlinhos comeu gelato de morango com creme de chocolate branco azulado de páscoa."))
 
+  //Another sintax could be:
+  // sum := sha256.Sum256([]byte("this is a password."))
+  // fmt.Printf("%x", sum)
+
   //Calculate and print the hash
   fmt.Printf("%x", h.Sum(nil))
 }
