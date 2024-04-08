30a9fa252263e062e2330fac0818c4a97925402d
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-08 19:54:03 -0300

    rsa key-pair

--- public-key/asymmetric.go
+++ public-key/asymmetric.go
@@ -16,4 +16,31 @@ func main() {
   if err != nil {
     panic(err)
   }
+
+  publicKey := &privateKey.PublicKey
+
+  privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+  privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+    Type: "RSA PRIVATE KEY",
+    Bytes: privateKeyBytes,
+  })
+
+  err = os.WriterFile("private.pem", privateKeyPEM, 0644)
+  if err != nil {
+    panic(err)
+  }
+
+  publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+  if err != nil {
+    panic(err)
+  }
+  publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+    Type: "RSA PUBLIC KEY",
+    Bytes: publicKeyBytes,
+  })
+
+  err = os.WriterFile("public.pem", publicKeyPEM, 0644)
+  if err != nil {
+    panic(err)
+  }
 }
