c0d630b9dafd4ddbafaaef97c1c99e8cb8c505f3
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-08 20:11:50 -0300

    encrypt code for key pair

---
+++ public-key/encrypt.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "crypto/rand"
+  "crypto/rsa"
+  "crypto/x509"
+  "encoding/pem"
+  "fmt"
+  "io/ioutil"
+)
+
+func main() {
+  publicKeyPEM, err := ioutil.ReadFile("public.pem")
+  if err != nil {
+    panic(err)
+  }
+
+  publicKeyBlock, _ := pem.Decode(publicKeyPEM)
+  publicKey, err := x509.ParsePKIXPublicKey(publicKeyBlock.Bytes)
+
+  if err != nil {
+    panic(err)
+  }
+
+  plaintext := []byte("hello, world!")
+  ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey.(*rsa.publicKey), plaintext)
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Printf("Encrypted: %x\n", ciphertext)
+}
