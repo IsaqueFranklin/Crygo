e0eb1ae53ec9eb7c39fbbd1e7afe5b7c8999d8cf
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-06-14 22:24:42 -0300

    more

--- aes/simpleCode.go
+++ aes/simpleCode.go
@@ -13,42 +13,46 @@ import (
 )
 
 func main() {
-  fmt.Println("Encryption Programa v0.01")
+    fmt.Println("Encryption Program v0.01")
 
-  text := []byte("My Super Secret Code Stuff")
-  key := []byte("passprahsewithlajnlsknlkc!")
+    text := []byte("My Super Secret Code Stuff")
+    key := []byte("passphrasewhichneedstobe32bytes!")
 
-  //Generate a new aes cipher using ou 32 byte long key
-  c, err := aes.NewCipher(key)
+    // generate a new aes cipher using our 32 byte long key
+    c, err := aes.NewCipher(key)
+    // if there are any errors, handle them
+    if err != nil {
+        fmt.Println(err)
+    }
 
-  //if there are any errors, handle them
-  if err != nil {
-    fmt.Println(err)
-  }
-  
-  // gcm or Galois/Counter Mode, is a mode of operation
+    // gcm or Galois/Counter Mode, is a mode of operation
     // for symmetric key cryptographic block ciphers
     // - https://en.wikipedia.org/wiki/Galois/Counter_Mode
+    gcm, err := cipher.NewGCM(c)
+    // if any error generating new GCM
+    // handle them
+    if err != nil {
+        fmt.Println(err)
+    }
 
-  gcm, err := cipher NewGCM(c)
+    // creates a new byte array the size of the nonce
+    // which must be passed to Seal
+    nonce := make([]byte, gcm.NonceSize())
+    // populates our nonce with a cryptographically secure
+    // random sequence
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        fmt.Println(err)
+    }
 
-  if err != nil {
-    fmt.Println(err)
-  }
-
-  //creates a new byte array the size of the nonce
-  //which must be passed to seal
-
-  nonce := make([]byte, gcm.NonceSize())
-  //Populates our nonce with a cryptographcally secure random sequence
-  if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-    fmt.Println(err)
-  }
-
-  // here we encrypt our text using the Seal function
+    // here we encrypt our text using the Seal function
     // Seal encrypts and authenticates plaintext, authenticates the
     // additional data and appends the result to dst, returning the updated
     // slice. The nonce must be NonceSize() bytes long and unique for all
     // time, for a given key.
-    fmt.Println(gcm.Seal(nonce, nonce, text, nil))
+    err = ioutil.WriteFile("myfile.data", gcm.Seal(nonce, nonce, text, nil), 0777)
+// handle this error
+if err != nil {
+  // print it out
+  fmt.Println(err)
+}
 }
